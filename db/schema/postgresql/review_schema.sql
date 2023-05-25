a682ba1a10205fecf1d3041dd14b167f2021cb3c
Milad Moulayi <m.moulayi123@gmail.com>
2023-05-25 15:15:45 -0700

    Moved all tables to a single schema file, edited import_csvs script file to execute regardless of the source of the csv file (was having issues access issues with my postgres user trying to access files in wsl user directory)

--- db/schema/postgresql/review_schema.sql
+++ db/schema/postgresql/review_schema.sql
@@ -18,3 +18,40 @@ CREATE TABLE IF NOT EXISTS reviewsList.review
     helpfulness INT DEFAULT 0 NOT NULL
 )
 TABLESPACE pg_default;
+
+DROP TABLE IF EXISTS reviewsList.photo;
+
+CREATE TABLE IF NOT EXISTS reviewsList.photo
+(
+	id SERIAL PRIMARY KEY NOT NULL,
+    review_id INT NOT NULL,
+    url TEXT NOT NULL
+);
+
+ALTER TABLE reviewsList.photo ADD CONSTRAINT review_id_fkey FOREIGN KEY (review_id) REFERENCES reviewsList.review(id);
+
+DROP TABLE IF EXISTS reviewsList.characteristics;
+
+CREATE TABLE IF NOT EXISTS reviewsList.characteristics
+(
+id BIGSERIAL PRIMARY KEY NOT NULL,
+review_id BIGINT NOT NULL,
+name TEXT NOT NULL
+)
+TABLESPACE pg_default;
+
+ALTER TABLE reviewsList.characteristics ADD CONSTRAINT review_id_fkey FOREIGN KEY (review_id) REFERENCES reviewsList.review(id);
+
+DROP TABLE IF EXISTS reviewsList.characteristic_reviews;
+
+CREATE TABLE IF NOT EXISTS reviewsList.characteristic_reviews
+(
+id BIGSERIAL PRIMARY KEY NOT NULL,
+characteristic_id BIGINT NOT NULL,
+review_id BIGINT NOT NULL,
+VALUE INT NOT NULL
+)
+TABLESPACE pg_default;
+
+ALTER TABLE reviewsList.characteristic_reviews ADD CONSTRAINT review_id_fkey FOREIGN KEY (review_id) REFERENCES reviewsList.review(id);
+ALTER TABLE reviewsList.characteristic_reviews ADD CONSTRAINT characteristic_id_fkey FOREIGN KEY (characteristic_id);REFERENCES reviewsList.characteristics(id);
--- db/scripts/import_csvs.sql
+++ db/scripts/import_csvs.sql
@@ -1,15 +1,7 @@
-COPY reviewsList.review
-FROM '/tmp/postgres/csvs/reviews.csv'
-DELIMITER ',' CSV HEADER;
+\COPY reviewsList.review FROM '/home/purplepenguinparty/hackreactor2303/sdc/reviews-api/db/csvs/reviews.csv' DELIMITER ',' CSV HEADER;
 
-COPY reviewsList.photo
-FROM '/tmp/postgres/csvs/reviews_photos.csv'
-DELIMITER ',' CSV HEADER;
+\COPY reviewsList.photo FROM '/home/purplepenguinparty/hackreactor2303/sdc/reviews-api/db/csvs/reviews_photos.csv' DELIMITER ',' CSV HEADER;
 
-COPY reviewsList.characteristics
-FROM '/tmp/postgres/csvs/characteristics.csv'
-DELIMITER ',' CSV HEADER;
+\COPY reviewsList.characteristics FROM '/home/purplepenguinparty/hackreactor2303/sdc/reviews-api/db/csvs/characteristics.csv' DELIMITER ',' CSV HEADER;
 
-COPY reviewsList.characteristic_reviews
-FROM '/tmp/postgres/csvs/characteristic_reviews.csv'
-DELIMITER ',' CSV HEADER;
+\COPY reviewsList.characteristic_reviews FROM '/home/purplepenguinparty/hackreactor2303/sdc/reviews-api/db/csvs/characteristic_reviews.csv' DELIMITER ',' CSV HEADER;
