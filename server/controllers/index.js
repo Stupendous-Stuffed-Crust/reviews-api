c168dc7f5e811af535b71f8b73bc0172c00ac575
Milad Moulayi <m.moulayi123@gmail.com>
2023-05-27 10:55:01 -0700

    Completed functioning getReviews endpoint with product_id, sortBy, limit, and page query parameters

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,9 +2,9 @@ const models = require('../models');
 
 module.exports = {
   get: function (req, res) {
-    const {product_id} = req.query;
-    models.getReviews(product_id)
-      .then((result) => res.status(200).send(result.rows))
+    const {product_id, sortBy, limit, page} = req.query;
+    models.getReviews(product_id, sortBy, limit, page)
+      .then((result) => res.send(result.rows))
       .catch((err) => res.status(500).send('Error executing query: ' + err.stack));
   },
 }
--- server/models/index.js
+++ server/models/index.js
@@ -1,11 +1,11 @@
 const {pool} = require('../db');
 
 module.exports = {
-  getReviews: function(product_id) {
+  getReviews: function(product_id, sortBy='id', limit=5, page=1) {
     const query = {
       name: 'fetch-product-reviews',
-      text: 'SELECT * from review WHERE product_id=$1',
-      values: [product_id]
+      text: `SELECT * from review WHERE product_id=$1 ORDER BY ${sortBy} ${sortBy === 'id' ? 'ASC' : 'DESC'} LIMIT $2 OFFSET $3`,
+      values: [product_id, limit, limit * (page - 1)]
     }
       return pool.query(query)
     },
